6565a2d4186c7956bc9400d56c8773593c38b444
LoneWald <54796460+LoneWald@users.noreply.github.com>
2021-11-22 11:01:02 +0300

    .

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class Enemy : MonoBehaviour
 {
     private const int MAX_HEALTH = 100;
+    public Transform target;
+    private NavMeshAgent agent;
     [SerializeField] private int health;
+    private void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateUpAxis = false;
+    }
 
     void Update()
     {
         if(health <= 0)
             GameObject.Destroy(gameObject);
+
+        agent.SetDestination(target.position);
+
+        
     }
 
     public void ChangeHealth(int value)
